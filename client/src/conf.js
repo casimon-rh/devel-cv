977495c4b1d4019aa0359961b7ece3c8e4472f2d
Carlos Eduardo Simon Zepeda <firesstiller@gmail.com>
2018-04-11 19:00:02 -0500

    osh

--- client/src/conf.js
+++ client/src/conf.js
@@ -1,4 +1,4 @@
 var config = {};
-config.apibaseurl = process.env.CV_API_BASE ||"http://localhost:3001/api/";
+config.apibaseurl = "https://csi-cv.herokuapp.com/api/";
 console.log('config.apibaseurl',config.apibaseurl);
 module.exports =  config;
